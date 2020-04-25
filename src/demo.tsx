b71e22c4d3405e8310d973436c7853f7a03142e7
Sovan Jana <sovan@witbybit.com>
2020-04-25 14:53:23 +0530

    - making component more customizable

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,25 +1,36 @@
 import React from "react";
 import {
-  List,
-  ListItem,
-  ListItemText,
-  Menu,
+  ButtonBase,
   MenuItem,
-  Box
+  Box,
+  Popper,
+  Grow,
+  Paper,
+  ClickAwayListener,
+  MenuList
 } from "@material-ui/core";
 
-const options = [
+const options: Array<string> = [
   "Latest Activity",
   "Order Date (Oldest First)",
   "Order Date (Newest First)"
 ];
 
-export default function SimpleListMenu() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+export default function Demo() {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
   const [selectedIndex, setSelectedIndex] = React.useState(1);
 
-  const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen);
+  };
+
+  const handleClose = (event: React.MouseEvent<EventTarget>) => {
+    if (anchorRef.current?.contains(event.target as HTMLElement)) {
+      return;
+    }
+
+    setOpen(false);
   };
 
   const handleMenuItemClick = (
@@ -27,48 +38,69 @@ export default function SimpleListMenu() {
     index: number
   ) => {
     setSelectedIndex(index);
-    setAnchorEl(null);
+    handleClose(event);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  function handleListKeyDown(event: React.KeyboardEvent) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current!.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
 
   return (
-    <Box
-      height="500px"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <List component="nav" aria-label="sort-by-filter">
-        <ListItem
-          button
-          aria-haspopup="true"
-          aria-controls="sort-by-menu"
-          aria-label="sort-by"
-          onClick={handleClickListItem}
-        >
-          <ListItemText primary={`Sort By : ${options[selectedIndex]}`} />
-        </ListItem>
-      </List>
-      <Menu
-        id="sort-by-menu"
-        anchorEl={anchorEl}
-        // keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
+    <Box height="300px" display="flex" alignItems="center">
+      <ButtonBase
+        ref={anchorRef}
+        aria-controls={open ? "menu-list-grow" : undefined}
+        aria-haspopup="true"
+        onClick={handleToggle}
       >
-        {options.map((option, index) => (
-          <MenuItem
-            key={option}
-            selected={index === selectedIndex}
-            onClick={event => handleMenuItemClick(event, index)}
-          >
-            {option}
-          </MenuItem>
-        ))}
-      </Menu>
+        {`Sort By : ${options[selectedIndex]}`}
+      </ButtonBase>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+        placement='bottom-start'
+      >
+        {({ TransitionProps }) => (
+          <Grow {...TransitionProps}>
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList
+                  autoFocusItem={open}
+                  id="menu-list-grow"
+                  onKeyDown={handleListKeyDown}
+                >
+                  {options.map((opt, index) => {
+                    return (
+                      <MenuItem
+                        onClick={event => handleMenuItemClick(event, index)}
+                        selected={index === selectedIndex}
+                      >
+                        {opt}
+                      </MenuItem>
+                    );
+                  })}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
     </Box>
   );
 }
